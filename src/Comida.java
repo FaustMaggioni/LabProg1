17d1e8abd9fdb57114706cd78130bf8d877ebec0
Faustino <faustino.maggioni@est.fi.uncoma.edu.ar>
2021-09-02 10:32:05 -0300

    cambios innecesarios

--- src/Comida.java
+++ src/Comida.java
@@ -1,8 +1,24 @@
 abstract public class Comida {
-    public void llamarDelivery(int distancia){
-        Repartidor rp = crearRepartidor(distancia);
+    protected double peso;
+    public void llamarDelivery(int cantidad){
+        Repartidor rp = crearRepartidor(cantidad);
         //ver si ejecutar el runnable o meterlo en el ScheduleExecutor
     }
 
-    protected abstract Repartidor crearRepartidor(int distancia);
+    protected Repartidor crearRepartidor(int cantidad){
+        double pesoTotal = getPesoTotal(cantidad);
+        if(pesoTotal < 100){
+            return new Bicicleta();
+        }
+        if(pesoTotal >= 100 && pesoTotal < 200){
+            return new Moto();
+        }
+        if(pesoTotal >= 200){
+            return new Auto();
+        }
+    }
+
+    protected double getPesoTotal(int cantidad){
+        return this.peso*cantidad;
+    }
 }
--- src/Milanesa.java
+++ src/Milanesa.java
@@ -1,15 +1,6 @@
 public class Milanesa extends Comida {
-    @Override
-    protected Repartidor crearRepartidor(int distancia) {
-        if(distancia < 50){
-            return new Bici();
-        }else{
-            if(distancia >= 50 && distancia < 100){
-                return new Moto();
-            }else{
-                return new Auto();
-            }
-        }
+    public Milanesa (double peso){
+        super();
+        this.peso = peso;
     }
-
 }
